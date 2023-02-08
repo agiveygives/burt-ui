f12843f6a5b459e021ccb209368ff2f0530ee209
agiveygives <justincase25xxv@gmail.com>
2023-02-08 01:19:59 -0600

    storybook

---
+++ types/stories/Button.stories.d.ts
@@ -0,0 +1,6 @@
+declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0a347bb9").R, import("./Button").ButtonProps>;
+export default _default;
+export declare const Primary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, import("./Button").ButtonProps>;
+export declare const Secondary: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, import("./Button").ButtonProps>;
+export declare const Large: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, import("./Button").ButtonProps>;
+export declare const Small: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, import("./Button").ButtonProps>;
